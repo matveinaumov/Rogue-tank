025936acb3377ee440b565a75f64b927dce47cae
Ihar <loliabc7@gmail.com>
2026-07-16 20:16:22 +0200

    map

--- scripts/map_renderer/renderMap.gd
+++ scripts/map_renderer/renderMap.gd
@@ -1,10 +1,21 @@
-extends Node
+extends TileMapLayer
 
 
-# Called when the node enters the scene tree for the first time.
+@export var width = 50
+@export var height = 30
+
+
+var ground_tile = Vector2i(0, 0)
+var wall_tile = Vector2i(1, 0)
+
+
 func _ready() -> void:
-	print("test")
-	pass # Replace with function body.
+	for y in range(height):
+		for x in range(width):
+			set_cell(Vector2(x,y), 0, wall_tile)
+			if randf() < 0.1:
+				set_cell(Vector2(x,y), 0, ground_tile)
+	pass 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
