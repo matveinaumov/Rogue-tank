c3f3ca246825ec159135665abc84a923d556884a
matveinaumov <p4ssw0rded@gmail.com>
2026-07-16 22:17:37 +0200

    igar sori za sprajty

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,15 @@
 extends CharacterBody2D
 
-@export var speed = 500
-func get_input():
-	var input_dir = Input.get_vector("left","right","up","down")
+@export var speed = 100
+@export var rot_speed = 3
+@export var rot_dir = 0
+
+func _physics_process(delta: float) -> void:
+	rot_dir = Input.get_axis("left","right")
+	velocity = transform.x * Input.get_axis("down","up") *speed
+	if velocity:
+		%AnimatedSprite2D.play("drive")
+	else:
+		%AnimatedSprite2D.stop()
+	rotation += rot_dir*rot_speed*delta
+	move_and_slide()
