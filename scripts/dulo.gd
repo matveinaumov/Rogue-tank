c81610142bf06750398f35dca568861646941ee9
matveinaumov <p4ssw0rded@gmail.com>
2026-07-17 12:33:55 +0200

    tanchik streliajet puliami

--- scripts/dulo.gd
+++ scripts/dulo.gd
@@ -1,11 +1,22 @@
 extends Node2D
-
+@onready var pulia_preload = preload("res://scenes/bullet.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func piupiu():
+	var pulia = pulia_preload.instantiate()
+	get_tree().current_scene.add_child(pulia)  # or get_parent(), or a Bullets node
+	pulia.global_position = %bullet_spawner.global_position
+	pulia.global_rotation = %dulo.global_rotation
+	%reload.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
+	print(%reload.time_left)
+	
+func _input(event: InputEvent) -> void:
+	if(event is InputEventMouseButton and event.is_pressed() and %reload.time_left ==0.0):
+		piupiu()
