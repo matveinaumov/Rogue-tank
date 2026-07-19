855519a4ab5c8495dd74521ce4b05a2c1279eca4
Ihar <loliabc7@gmail.com>
2026-07-19 20:18:34 +0200

    End random

--- scripts/map_renderer/renderMap.gd
+++ scripts/map_renderer/renderMap.gd
@@ -1,11 +1,11 @@
 extends TileMapLayer
 
 
-@export var _Path : int = 5
+@export var _Path : int = randi_range(10,20)
 
 @export var _Size_Dunger : Vector2i = Vector2i(50,30)
 
-@export var rooms : Array = []
+@export var rooms : Array
 
 @export var list = [
 	Vector2i(0, 0), # road
@@ -13,10 +13,14 @@ extends TileMapLayer
 	Vector2i(2, 0), # wall
 	Vector2i(3, 0)  # water
 ]
+
+
+# ----------------------------------------------------------------------------------
 class Room:
 	var size_room : Vector2i
-	var Way_room : Array[String]
+	var Way_room : Dictionary = {}
 	var cell : Array
+	
 	
 	func _init(s: Vector2i):
 		size_room = s
@@ -34,22 +38,86 @@ class Room:
 					cell[y].append(Vector2(0,0))
 
 
+
+# ---------------------------------------------------------------------------------
+
+func get_max(r : Array) -> int:
+	var out : int
+	for i in r:
+		var mm = max(i.x, i.y)
+		if out < mm:
+			out = mm
+	return out
+
+
+func ADD_Rooms(r : Array):
+	var pos = generate_room_positions(_Path)
+	Rooms_in(pos.size())
+	for i in range(pos.size()):
+		for j in range(i + 1, pos.size()):
+			var diff = pos[j] - pos[i]
+			if diff == Vector2i.RIGHT:
+				r[i].Way_room[j] = "Right"
+				r[j].Way_room[i] = "Left"
+			elif diff == Vector2i.LEFT:
+				r[i].Way_room[j] = "Left"
+				r[j].Way_room[i] = "Right"
+			elif diff == Vector2i.DOWN:
+				r[i].Way_room[j] = "Down"
+				r[j].Way_room[i] = "Up"
+			elif diff == Vector2i.UP:
+				r[i].Way_room[j] = "Up"
+				r[j].Way_room[i] = "Down"
 	
+	#for i in range(r.size()):
+		#print(i, r[i].Way_room)
+
+func Rooms_in(count : int):
+	for i in range(count):
+		rooms.append(Room.new(_Size_Dunger))
 
 
-func Draw(room : Room) -> void:
+func generate_room_positions(count:int) -> Array:
+	var positions : Array = [Vector2i.ZERO]
+	var r = {Vector2i.ZERO : true}
+
+	while positions.size() < count:
+		var base = positions.pick_random()
+		var dir : Vector2i
+		
+		match randi_range(0,3):
+			0: dir = Vector2i.UP
+			1: dir = Vector2i.DOWN
+			2: dir = Vector2i.LEFT
+			3: dir = Vector2i.RIGHT
+		
+		var pos = base + dir
+		if pos.x < 0 or pos.y < 0:
+			continue
+
+		if r.has(pos):
+			continue
+
+		r[pos] = true
+		positions.append(pos)
+
+	return positions
+
+func Room_Draw(room : Room) -> void:
 	for y in range(room.size_room.y):
 		for x in range(room.size_room.x):
 			set_cell(Vector2i(x,y), 0,room.cell[y][x])
 
-func _Rooms() -> void:
-	for i in range(_Path):
-		rooms.append(Room.new(_Size_Dunger))
+func Draw(room : Array, id : int):
+	Room_Draw(room[id])
+
+
+# ----------------------------------------------------------------------------------
 
 
 func _ready() -> void:
-	_Rooms()
-	Draw(rooms[0])
+	
+	ADD_Rooms(rooms)
 	pass
 	
 	
